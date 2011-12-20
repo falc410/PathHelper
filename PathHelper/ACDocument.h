c1900cd669f1047dd9f36a5d4a74ea2e3d71db26
Tobias Appel <tobias@appel-computer.com>
2011-12-20 12:56:44 +0100

    Initial Commit

---
+++ PathHelper/ACDocument.h
@@ -0,0 +1,13 @@
+//
+//  ACDocument.h
+//  PathHelper
+//
+//  Created by Tobias Appel on 20.12.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ACDocument : NSDocument
+
+@end
